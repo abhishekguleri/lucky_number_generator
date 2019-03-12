82b53af334313e7e79a1b8489abc1940b18166e5
Abhishek Guleri <43719098+abhishekguleri@users.noreply.github.com>
2019-03-12 22:49:57 +0530

    Update Main.java

--- src/Guleri/Main.java
+++ src/Guleri/Main.java
@@ -1,21 +1,34 @@
 package Guleri;
+
+//lucky number generator;
 import java.util.Scanner;
-public class Main {
+import javax.swing.JOptionPane;
+public class Main
+{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("\033[1;34m Do you want to play the game of luck.\n" +
+                "Are you ready guys.y or n\n\033[0m");
+        String y_n = input.nextLine();
+        if (y_n.equals("y") || y_n.equals("Y")) {
+            System.out.println(" enter your favourite two numbers");
+            int num = input.nextInt();
+            num = num + 10 / 5;
+            int num1 = input.nextInt();
+            num1 = num + 9 * 30;
+            System.out.println("\033[1;32m this is your lucky number:" + num1);
 
 
-            int rows, num = 1, i, j;
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter the number of rows for floyd's triangle:");
-            rows = input.nextInt();
-            System.out.println("Floyd's triangle");
-            System.out.println("---------------------------");
-            for (i= 1; i<= rows; i++) {
-                for (j = 1; j <= i; j++) {
-                    System.out.print(num + " ");
-                    num++;
-                }
-                System.out.println();
+
             }
+         else {
+            System.out.println("thanks to visit");
         }
+
+                String name = JOptionPane.showInputDialog("welcome,enter your name");
+                String message =String.format(name+"thank you");
+
     }
+
+
+}
